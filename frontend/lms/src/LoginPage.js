e92a8cc2c90d500eac97ee87eca2e0a1b69eb11a
Justin <justin.vuong6@gmail.com>
2025-04-08 21:20:57 -0600

    fixed css and regex

--- frontend/lms/src/LoginPage.js
+++ frontend/lms/src/LoginPage.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import LoginForm from './components/LoginForm';
-import './components/LoginForm.css';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import LoginForm from "./components/LoginForm";
+import "./components/LoginForm.css";
+import "./LoginPage.css";
 
 const LoginPage = () => {
-    return (
-        <div>
-            <Header />
-            <LoginForm />
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="login-page">
+      <Header />
+      <LoginForm />
+      <Footer />
+    </div>
+  );
 };
 
 export default LoginPage;
--- frontend/lms/src/SignupPage.js
+++ frontend/lms/src/SignupPage.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import RegForm from './components/RegForm';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import RegForm from "./components/RegForm";
 
 const SignupPage = () => {
-    return (
-        <div>
-            <Header />
-            <RegForm />
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="sign-up-page">
+      <Header />
+      <RegForm />
+      <Footer />
+    </div>
+  );
 };
 
 export default SignupPage;
